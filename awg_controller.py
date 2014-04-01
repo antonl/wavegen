a425b18aff935638da675c19e2e698ca621ae0e0
Ogilvie Group <ogilviegroupgithub@gmail.com>
2014-03-31 20:24:34 -0400

    WIP: the controller works, but buttons don't get disabled and logging doesn't work like it should. I suspect it is some sort of thread/inversion of control issue.

--- awg_controller.py
+++ awg_controller.py
@@ -2,6 +2,8 @@ from __future__ import division, print_function
 import statemachine as fsm
 import logging
 import numpy as np
+import sys
+from PySide import QtGui
 
 log = logging.getLogger('wavegen')
 log.addHandler(logging.NullHandler())
@@ -49,7 +51,6 @@ def generate_waveform(phase=4, plot=False):
 
 try:
     import visa
-    _rm = visa.ResourceManager()
 except ImportError as e:
     log.error('please install pyvisa package')
     raise e
@@ -59,8 +60,8 @@ except OSError:
 class Agilent332xxError(Exception): pass
 
 class Agilent332xx(object):
-    def __init__(self, name, **kwargs):
-        self.me = _rm.get_instrument(name, **kwargs)
+    def __init__(self, instrument, **kwargs):
+        self.me = instrument
 
         try:
             self.ask('*idn?')
@@ -81,6 +82,9 @@ class Agilent332xx(object):
     def send_waveform_macro(self):
         log.info('generating waveform')
         self.waveform = generate_waveform()
+
+        log.info('clearing volatile memory')
+        self.write('data:vol:cle')
 
         log.info('sending waveform')
 
@@ -174,8 +178,27 @@ class WavegenController(fsm.Machine):
     @fsm.event
     def init(self):
         self.reason = ''
+        self.ResourceManager = visa.ResourceManager()
         self.instruments = visa.get_instruments_list()
         
+        # Create application
+        self.app = QtGui.QApplication(sys.argv)
+
+        # Create GUI
+        self.gui = WavegenGui()
+        # Connect log
+        #self.log_handler = GuiLoggingHandler(self.gui.log)
+        #self.log_handler.setFormatter(HtmlFormatter())
+        #self.log_handler.setLevel(logging.DEBUG)
+        #log.addHandler(self.log_handler)
+        # Set instruments
+        self.gui.device_chooser.addItems(self.instruments)
+        # Connect events
+        self.connect_events()
+        
+        # Show dialog
+        self.gui.show()
+
         if len(self.instruments) < 1: 
             success = False
             self.reason = 'did not get any instruments from visa'
@@ -188,10 +211,28 @@ class WavegenController(fsm.Machine):
         else:
             yield 'preinit', 'fail_init'
 
+    @fsm.after_transition('preinit', 'wait1')
+    def invert_control(self):
+        sys.exit(self.app.exec_())
+
+    def connect_events(self):
+        g = self.gui
+        g.device_chooser.currentIndexChanged.connect(self.set_device)
+        g.load_waveform_btn.clicked.connect(self.load_waveform)
+        g.delay_box.valueChanged.connect(self.set_delay)
+        g.voltage_box.valueChanged.connect(self.set_output_voltage)
+        g.enable_box.stateChanged.connect(self.handle_enable)
+    
+    def handle_enable(self):
+        if self.gui.enable_box.isChecked():
+            self.enable()
+        else:
+            self.disable()
+
     @fsm.event
     def set_device(self, id):
         try:
-            self.instrument = Agilent332xx(self.instruments[id])
+            self.instrument = Agilent332xx(visa.instrument(self.instruments[id]))
             self.instrument.send_reset_macro()
             success = True
         except visa.VisaIOError as e:
@@ -267,3 +308,75 @@ class WavegenController(fsm.Machine):
     def log_reason(self):
         log.error(self.reason)
 
+class HtmlFormatter(logging.Formatter):
+    def __init__(self, *args, **kwargs):
+        logging.Formatter.__init__(self, *args, **kwargs)
+
+    def format(record):
+        return "this is a record!"
+        formatted = logging.Formatter.format(record)
+
+        if record.level <= logging.DEBUG:
+            color = "#fee5d9"
+        elif record.level <= logging.INFO:
+            color = "#fcae91"
+        elif record.level <= logging.WARNING:
+            color = "#fb6a4a"
+        elif record.level <= logging.CRITICAL:
+            color = "#cb181d"
+        else:
+            color = "#ffffff"
+
+        res = "<p style=\"color: {color:s}\">{msg:s}</p>".format(
+            color, formatted)
+        print(res)
+        return res
+
+class GuiLoggingHandler(logging.Handler):
+    def __init__(self, target, *args, **kwargs):
+        logging.Handler.__init__(self, *args, **kwargs)
+        self.target = target
+
+    def emit(self, record):
+        formatted = logging.Handler.format(self, record)
+        print(formatted)
+        self.append(formatted)
+
+class WavegenGui(QtGui.QMainWindow):
+    def __init__(self, *args, **kwargs):
+        QtGui.QMainWindow.__init__(self, *args, **kwargs)
+        self.make_main_frame()
+    
+    def make_main_frame(self):
+        self.setWindowTitle("Stark Waveform Controller")
+        self.main_frame = QtGui.QWidget()
+        
+        hgroup = QtGui.QGroupBox("Instructions")
+        hlayout = QtGui.QHBoxLayout()
+
+        #self.log = QtGui.QTextEdit()
+        #for w in [hgroup, self.log]:
+        #    hlayout.addWidget(w)
+        hlayout.addWidget(hgroup)
+
+        form = QtGui.QFormLayout()
+        
+        self.device_chooser = QtGui.QComboBox()
+        self.load_waveform_btn = QtGui.QPushButton("Send!")
+        self.delay_box = QtGui.QDoubleSpinBox()
+        self.voltage_box = QtGui.QDoubleSpinBox()
+        self.enable_box = QtGui.QCheckBox("Enabled")
+
+        form.addRow("1. Choose device", self.device_chooser)
+        form.addRow("2. Send waveform", self.load_waveform_btn)
+        form.addRow("3. Set delay (us)", self.delay_box)
+        form.addRow("4. Set output volage", self.voltage_box)
+        form.addRow("5. Enable output", self.enable_box)
+        
+        hgroup.setLayout(form)
+        self.main_frame.setLayout(hlayout)
+        self.setCentralWidget(self.main_frame)
+
+if __name__ == '__main__':
+    ctrl = WavegenController()
+    ctrl.init()
